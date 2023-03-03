b9a1e4103dbfa191aab4b2496613a8e11f3dc365
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2023-03-03 19:07:58 +0400

    first commit

---
+++ wp-starter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Starter
+ * Plugin URI:      https://github.com/w7shdev/wp-plugin-starter
+ * Description:     Starter plugin created by using wp-cli & laravel-mix
+ * Author:          W7shdev
+ * Author URI:      https://github.com/w7shdev
+ * Text Domain:     wp-starter
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Starter
+ */
+
+// Your code starts here.
